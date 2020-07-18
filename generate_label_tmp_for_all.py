d6fb6b4ec1bc7b056ea778ff7ea0a54989391b02
Mark Huang <hhzeos@gmail.com>
2020-07-18 16:12:34 +0800

    Update

--- generate_label_tmp_for_all.py
+++ generate_label_tmp_for_all.py
@@ -13,6 +13,13 @@ def main():
         assert folder_path.is_dir()
         sublist_fp: Path = folder_path / "video_metadata_lst.json"
         assert sublist_fp.is_file()
+        with sublist_fp.open() as f:
+            vid_lst: list = json.load(f)
+            name_list = [f'{video["filename"]+video["ext"]}' for video in vid_lst]
+
+        for video in os.listdir(folder_path):
+            assert video in name_list
+            print("True")
 
 
 if __name__ == "__main__":
