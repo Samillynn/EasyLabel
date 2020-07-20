d6ef8bc8abdb127cf9275c4b509080d718050a7f
Mark Huang <hhzeos@gmail.com>
2020-07-21 00:37:18 +0800

    Bug Fix

--- generate_template.py
+++ generate_template.py
@@ -76,7 +76,8 @@ def generate_qa_template_from_json(json_fp: str):
         )
 
     with export_fp.open(mode="w") as f:
-        f.write("\n\n".join(string_lst_to_write))
+        to_write = str("\n\n".join(string_lst_to_write))
+        f.write(to_write)
     print(f"successfully generated: {export_fp}")
 
 
