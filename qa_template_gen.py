490ec514bb5c4bc0d8c85c8f42bff034cce78b6f
Mark Huang <hhzeos@gmail.com>
2020-07-29 19:46:27 +0800

    Update qa_template_gen.py

--- qa_template_gen.py
+++ qa_template_gen.py
@@ -6,10 +6,15 @@ from my_logger import logger as _logger
 from qa_class import QASet, QASetPool, get_qa_pool_from_json
 from markkk.pyutils.check_text_encoding import is_ascii, ensure_no_zh_punctuation
 
+# high frequency QASet pool
 QA_BANK_JSON_FILEPATH = "qa_bank/26July_high_quality.json"
-QA_BANK_JSON_FILEPATH_2 = "qa_bank/qa_bank_20_July_2020.json"
-qa_pool: QASetPool = get_qa_pool_from_json(QA_BANK_JSON_FILEPATH)
-qa_pool_2: QASetPool = get_qa_pool_from_json(QA_BANK_JSON_FILEPATH_2)
+hf_qa_pool: QASetPool = get_qa_pool_from_json(QA_BANK_JSON_FILEPATH)
+
+# low frequency QASet pool
+QA_BANK_JSON_FILEPATH = "qa_bank/qa_bank_20_July_2020.json"
+lf_qa_pool: QASetPool = get_qa_pool_from_json(QA_BANK_JSON_FILEPATH)
+
+
 qa_section = """
 --------------------{{  }} # [1-6] or [d|e|p|r|c|i]
 <QASet_ID>: {{ None }}
@@ -18,26 +23,11 @@ qa_section = """
 """
 
 
-def auto_populated_qa_section(q_num: int, type=None) -> str:
+def auto_populated_qa_section(qa_pool: QASetPool, q_num: int, type=None) -> str:
+    assert isinstance(qa_pool, QASetPool)
+
     qa_sections: str = ""
     qa_sets: Set[QASet] = qa_pool.random_draw_multiple(q_num=q_num, type=type)
-
-    for qa_set in qa_sets:
-        qn, options = qa_set.get()
-        q_type_char: str = qa_set.type[0]
-        qa_sections += "--------------------{{ " + q_type_char + " }}\n"
-        qa_sections += "<QASet_ID>: {{ auto }}\n"
-        qa_sections += "<ANS>: {{  }}\n"
-        qa_sections += f"{qn}\n"
-        for option in options:
-            qa_sections += f"{option}\n"
-        qa_sections += "\n"
-
-    return qa_sections
-
-def auto_populated_qa_section_2(q_num: int, type=None) -> str:
-    qa_sections: str = ""
-    qa_sets: Set[QASet] = qa_pool_2.random_draw_multiple(q_num=q_num, type=type)
 
     for qa_set in qa_sets:
         qn, options = qa_set.get()
@@ -63,8 +53,8 @@ def template_video_section(filename: str, duration: str, dimension: str) -> str:
     vid_section += "<PERSPECTIVE>: {{  }}\n"
     vid_section += "<RE_TRIM>: {{ START_TS, END_TS }}\n"
     vid_section += "<CRITICAL_POINT>: {{ TS }}\n\n"
-    vid_section += auto_populated_qa_section(3)
-    vid_section += auto_populated_qa_section_2(4)
+    vid_section += auto_populated_qa_section(qa_pool=hf_qa_pool, q_num=4)
+    vid_section += auto_populated_qa_section(qa_pool=lf_qa_pool, q_num=3)
     vid_section += qa_section * 3
 
     return vid_section
