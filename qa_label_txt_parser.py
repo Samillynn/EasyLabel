09075411f3850aec82c81df5fc1515d9342e2c4d
Mark Huang <hhzeos@gmail.com>
2020-07-31 01:39:48 +0800

    Major update on logging msgs

--- qa_label_txt_parser.py
+++ qa_label_txt_parser.py
@@ -18,6 +18,29 @@ ans_map = {
     "K": 10,
     "L": 11,
 }
+supported_types = (
+    "Descriptive",
+    "Explanatory",
+    "Predictive",
+    "Reverse Inference",
+    "Counterfactual",
+    "Introspection",
+)
+
+abbr_to_qtype_map = {
+    "d": "Descriptive",
+    "e": "Explanatory",
+    "p": "Predictive",
+    "r": "Reverse Inference",
+    "c": "Counterfactual",
+    "i": "Introspection",
+    "1": "Descriptive",
+    "2": "Explanatory",
+    "3": "Predictive",
+    "4": "Reverse Inference",
+    "5": "Counterfactual",
+    "6": "Introspection",
+}
 
 
 def get_value(line: str) -> str:
@@ -31,12 +54,13 @@ def get_value(line: str) -> str:
         _logger.error(f"`{line}` does not satisfy expected format")
         return
     value = line[start:end].strip()
-    if value in ("None", "none", "nan", "NONE", "auto"):
+    if value in ("None", "none", "nan", "NONE"):
         value = None
     return value
 
 
-def qa_section_parser(qa_section: List) -> Dict:
+def qa_section_parser(qa_section: List) -> Tuple[Dict, bool]:
+    _is_valid = True
     q_ignore = False  # wether skip this qa section
     q_type = None  # store parsed question type
     q_sub = None  # store parsed QASet_ID
@@ -52,8 +76,20 @@ def qa_section_parser(qa_section: List) -> Dict:
             q_ignore = True
         elif line.startswith("---------"):
             q_type: str = get_value(line)
+            if q_type and q_type == "auto":
+                q_type = abbr_to_qtype_map.get("d")
+
+            elif q_type and q_type != "auto":
+                q_type = abbr_to_qtype_map.get(q_type.lower(), None)
+                if not q_type:
+                    _is_valid = False
+                    _logger.error("Invalid Question Type")
+                    q_type = "error"
+
         elif line.startswith("<QASet_ID>"):
             q_sub: str = get_value(line)
+            if q_sub in ("auto", "Auto", "AUTO"):
+                q_sub = None
             if q_sub:
                 sub = " "
                 q_body = sub
@@ -62,11 +98,13 @@ def qa_section_parser(qa_section: List) -> Dict:
             for char in ans.upper():
                 if char in ans_map:
                     ans_indexes.add(ans_map[char])
+
         else:
             if not q_sub and "?" in line:
                 # get question line
                 q_body = line.strip()
             elif q_sub and "?" in line:
+                _is_valid = False
                 _logger.error(
                     "Conflict: Question line detected while <QASet_ID> also presents."
                 )
@@ -84,6 +122,14 @@ def qa_section_parser(qa_section: List) -> Dict:
     if len(ans_indexes) == 0 and len(plus_indexes) == 0:
         q_ignore = True
 
+    if not q_ignore:
+        if not q_type:
+            _is_valid = False
+            _logger.error("Missing Question Type")
+        if not q_sub and not q_body:
+            _is_valid = False
+            _logger.error("Missing Question Line")
+
     # store parsed data
     qa_section_data: Dict = {
         "q_ignore": q_ignore,
@@ -96,7 +142,12 @@ def qa_section_parser(qa_section: List) -> Dict:
         "plus_indexes": tuple(plus_indexes),
     }
 
-    return qa_section_data
+    if not q_ignore and not _is_valid:
+        _logger.warning(
+            f"Error above occured at this QA: {json.dumps(qa_section_data, indent=2)}"
+        )
+
+    return qa_section_data, _is_valid
 
 
 def valid_time_format(time_string: str) -> bool:
@@ -116,7 +167,9 @@ def valid_time_format(time_string: str) -> bool:
     return False
 
 
-def vid_section_parser(vid_section: List) -> Dict:
+def vid_section_parser(vid_section: List) -> Tuple[Dict, bool]:
+    _is_valid = True
+    v_ignore = False
     # traverse thru vid_section line by line
     # and store the indexes where qa_sections starts
     qa_section_indexes = []
@@ -133,17 +186,35 @@ def vid_section_parser(vid_section: List) -> Dict:
     re_trim_ts = None
     critical_ts = None
     for line in vid_info_section:
-        if line.startswith("~~~~~~~~~~~~~~~~~~~~ "):
-            filename = line.strip("~").strip()
+        if line.startswith("!~~~~~~~~~~~~~~~~"):
+            v_ignore = True
+            filename = line.strip("!").strip("~").strip()
             if not filename:
+                _is_valid = False
                 _logger.error(f"Missing video filename")
             else:
                 print()
-                _logger.debug(f"~~~~~~~~ video section: {filename}")
+                _logger.debug(f"Checking video section: {filename} >>>>")
+                _logger.warning("This video is being ignored")
+            break
+        elif line.startswith("~~~~~~~~~~~~~~~~~~~~ "):
+            filename = line.strip("~").strip()
+            if not filename:
+                _is_valid = False
+                _logger.error(f"Missing video filename")
+            else:
+                print()
+                _logger.debug(f"Checking video section: {filename} >>>>")
         elif line.startswith("<PERSPECTIVE>"):
             perspective = get_value(line)
-            if not perspective:
-                _logger.error(f"Missing required <PERSPECTIVE> for {filename}")
+            if perspective:
+                if perspective not in ("1", "3"):
+                    _is_valid = False
+                    _logger.error(f"Unexpected value for <PERSPECTIVE>: {perspective}")
+            else:
+                _is_valid = False
+                _logger.error(f"Missing value for <PERSPECTIVE>.")
+
         elif line.startswith("<RE_TRIM>"):
             re_trim_ts = get_value(line)
             if re_trim_ts == "START_TS, END_TS":
@@ -152,6 +223,7 @@ def vid_section_parser(vid_section: List) -> Dict:
                 try:
                     start_ts, end_ts = tuple(re_trim_ts.split(","))
                 except:
+                    _is_valid = False
                     _logger.error(f"Error parsing <RE_TRIM> timestamps: {re_trim_ts}")
                     start_ts, end_ts = None, None
 
@@ -160,6 +232,7 @@ def vid_section_parser(vid_section: List) -> Dict:
                     start_ts = "0"
                 elif start_ts:
                     if not valid_time_format(start_ts):
+                        _is_valid = False
                         _logger.error(
                             f"Invalid START_TS Format for <RE_TRIM>: {start_ts}"
                         )
@@ -171,6 +244,7 @@ def vid_section_parser(vid_section: List) -> Dict:
                     pass
                 elif end_ts:
                     if not valid_time_format(end_ts):
+                        _is_valid = False
                         _logger.error(f"Invalid END_TS Format for <RE_TRIM>: {end_ts}")
                         end_ts = None
 
@@ -180,6 +254,7 @@ def vid_section_parser(vid_section: List) -> Dict:
                 critical_ts = None
             else:
                 if not valid_time_format(critical_ts):
+                    _is_valid = False
                     _logger.error(
                         f"Invalid Time Format for <CRITICAL_POINT>: {critical_ts}"
                     )
@@ -187,29 +262,99 @@ def vid_section_parser(vid_section: List) -> Dict:
 
     ### Parsing of qa sections
     qa_list: List[Dict] = []
-    for index, section_start in enumerate(qa_section_indexes):
-        if index < len(qa_section_indexes) - 1:
-            qa_section: List = vid_section[
-                section_start : qa_section_indexes[index + 1]
-            ]
-        else:
-            qa_section: List = vid_section[section_start:]
+    if not v_ignore:
+        for index, section_start in enumerate(qa_section_indexes):
+            if index < len(qa_section_indexes) - 1:
+                qa_section: List = vid_section[
+                    section_start : qa_section_indexes[index + 1]
+                ]
+            else:
+                qa_section: List = vid_section[section_start:]
 
-        qa_list.append(qa_section_parser(qa_section))
+            qa_section_data, valid_qa_section = qa_section_parser(qa_section)
+            qa_list.append(qa_section_data)
+            if not valid_qa_section:
+                _is_valid = False
 
     vid_section_data: Dict = {
+        "v_ignore": v_ignore,
         "filename": filename,
         "perspective": perspective,
         "re_trim_ts": re_trim_ts,
         "critical_ts": critical_ts,
         "qa_list": qa_list,
     }
-    # _logger.debug(json.dumps(vid_section_data))
 
-    return vid_section_data
+    return vid_section_data, _is_valid
+
+
+def get_stat(qa_label_lst: List[Dict]) -> Dict:
+    num_video_sections = len(qa_label_lst)
+    num_video_ignore = 0
+    num_video_require_retrim = 0
+    num_video_has_critical_point = 0
+    total_num_qns = 0
+    total_num_ops = 0
+    q_type_count_map: Dict = {}
+    q_body_count_map: Dict = {}
+
+    for video_section in qa_label_lst:
+        if video_section.get("v_ignore"):
+            num_video_ignore += 1
+        if video_section.get("re_trim_ts"):
+            num_video_require_retrim += 1
+        if video_section.get("critical_ts"):
+            num_video_has_critical_point += 1
+
+        qa_list: List[Dict] = video_section.get("qa_list")
+        for qa_section in qa_list:
+            is_ignored: bool = qa_section.get("q_ignore")
+            if not is_ignored:
+                total_num_qns += 1
+                q_type = qa_section.get("q_type")
+                if q_type in q_type_count_map:
+                    q_type_count_map[q_type] += 1
+                else:
+                    q_type_count_map[q_type] = 1
+
+                q_body = qa_section.get("q_body")
+                if q_body in q_body_count_map:
+                    q_body_count_map[q_body] += 1
+                else:
+                    q_body_count_map[q_body] = 1
+
+                total_num_ops += len(qa_section.get("option_lst"))
+
+    # derived values
+    num_video_labelled = num_video_sections - num_video_ignore
+    average_num_qns_per_video = (
+        total_num_qns / num_video_labelled if num_video_labelled != 0 else 0
+    )
+    average_num_ops_per_qns = total_num_ops / total_num_qns if total_num_qns != 0 else 0
+    num_of_q_type = len(q_type_count_map)
+    num_of_unique_qns = len(q_body_count_map)
+
+    stats: Dict = {
+        "num_video_sections": num_video_sections,
+        "num_video_ignore": num_video_ignore,
+        "num_video_require_retrim": num_video_require_retrim,
+        "num_video_has_critical_point": num_video_has_critical_point,
+        "total_num_qns": total_num_qns,
+        "total_num_ops": total_num_ops,
+        "q_type_count_map": q_type_count_map,
+        "q_body_count_map": q_body_count_map,
+        # derived
+        "num_video_labelled": num_video_labelled,
+        "average_num_qns_per_video": average_num_qns_per_video,
+        "average_num_ops_per_qns": average_num_ops_per_qns,
+        "num_of_q_type": num_of_q_type,
+        "num_of_unique_qns": num_of_unique_qns,
+    }
+    return stats
 
 
 def parse_qa_label_txt(txt_fp: str, writeToJson=False) -> List[Dict]:
+    label_file_is_valid = True
     # store parsed result
     qa_label_lst: List[Dict] = []
     # ensure txt file path is valid
@@ -228,7 +373,8 @@ def parse_qa_label_txt(txt_fp: str, writeToJson=False) -> List[Dict]:
     # look for video sections
     for index, line in enumerate(lines):
         line: str = line.strip()
-        if line.startswith("~~~~~~~~~~~~~~~~~~~~ "):
+        # if line.startswith("~~~~~~~~~~~~~~~~~~~~ "):
+        if "~~~~~~~~~~~~~~~~~" in line:
             vid_section_indexes.append(index)
 
     num_video_sections = len(vid_section_indexes)
@@ -241,20 +387,34 @@ def parse_qa_label_txt(txt_fp: str, writeToJson=False) -> List[Dict]:
         else:
             vid_section: List = lines[section_start:]
 
-        qa_label_lst.append(vid_section_parser(vid_section))
+        vid_section_data, _valid = vid_section_parser(vid_section)
+        qa_label_lst.append(vid_section_data)
+        if not _valid:
+            label_file_is_valid = False
 
-    if writeToJson:
-        export_fp: Path = txt_fp.parent / "qa.json"
+    if label_file_is_valid:
+        stats: Dict = get_stat(qa_label_lst)
+        _logger.info(json.dumps(stats, indent=4))
+
+    else:
+        print()
+        _logger.warning(
+            f"Your Label txt file ({txt_fp}) contains some errors, please check non-green error logs above.\n"
+        )
+
+    if writeToJson and label_file_is_valid:
+        export_fp: Path = txt_fp.parent / "qa_label.json"
         with export_fp.open(mode="w") as f:
             f.write(json.dumps(qa_label_lst, indent=4))
             _logger.debug(f"Successfully exported: {export_fp}")
+    elif writeToJson and not label_file_is_valid:
+        print()
+        _logger.warning(f"No json file is generated.")
 
     return qa_label_lst
 
 
 if __name__ == "__main__":
     parse_qa_label_txt(
-        "/Volumes/T5-SSD-Markkk/bilibili_003/qa_label_template.txt",
-        # verbose=False,
-        writeToJson=False,
+        "/Volumes/T5-SSD-Markkk/bilibili_003/qa_label_template.txt", writeToJson=True,
     )
