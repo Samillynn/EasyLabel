d7a9ae0d9f9db5979ef6ccf2841c2e9359bbb8dc
Mark Huang <hhzeos@gmail.com>
2020-08-17 21:50:53 +0800

    Update

--- label_file_parser.py
+++ label_file_parser.py
@@ -117,7 +117,9 @@ def qa_section_parser(
             correct_ans.add(an_answer)
         except IndexError:
             _is_valid = False
-            logger.error("Given correct answer out of range.")
+            logger.error(
+                "Given correct answer out of range, please check your value for <ANS>."
+            )
 
     # store parsed data
     if len(ans_indexes) == 0:
@@ -292,91 +294,6 @@ def vid_section_parser(
     return vid_section_data, _is_valid
 
 
-def get_stat(qa_label_lst: List[Dict]) -> Dict:
-    num_video_sections = len(qa_label_lst)
-    num_video_ignore = 0
-    num_video_require_retrim = 0
-    num_video_has_critical_point = 0
-    total_num_of_chars_in_qn_body = 0
-    total_num_of_words_in_qn_body = 0
-    total_num_qns = 0
-    total_num_ops = 0
-    q_type_count_map: Dict = {}
-    q_body_count_map: Dict = {}
-
-    for video_section in qa_label_lst:
-        if video_section.get("v_ignore"):
-            num_video_ignore += 1
-        if video_section.get("re_trim_ts"):
-            num_video_require_retrim += 1
-        if video_section.get("critical_ts"):
-            num_video_has_critical_point += 1
-
-        qa_list: List[Dict] = video_section.get("qa_list")
-        for qa_section in qa_list:
-            is_ignored: bool = qa_section.get("q_ignore")
-            if not is_ignored:
-                total_num_qns += 1
-                q_type = qa_section.get("q_type")
-                if q_type in q_type_count_map:
-                    q_type_count_map[q_type] += 1
-                else:
-                    q_type_count_map[q_type] = 1
-
-                q_body = qa_section.get("q_body")
-                total_num_of_chars_in_qn_body += len(q_body)
-                total_num_of_words_in_qn_body += len(q_body.split())
-                if q_body in q_body_count_map:
-                    q_body_count_map[q_body] += 1
-                else:
-                    q_body_count_map[q_body] = 1
-
-                total_num_ops += len(qa_section.get("option_lst"))
-
-    # derived values
-    num_video_labelled = num_video_sections - num_video_ignore
-
-    average_num_qns_per_video = (
-        total_num_qns / num_video_labelled if num_video_labelled != 0 else 0
-    )
-    average_num_qns_per_video = round(average_num_qns_per_video, 1)
-
-    average_num_ops_per_qn = total_num_ops / total_num_qns if total_num_qns != 0 else 0
-    average_num_ops_per_qn = round(average_num_ops_per_qn, 1)
-
-    average_num_chars_per_qn = (
-        total_num_of_chars_in_qn_body / total_num_qns if total_num_qns != 0 else 0
-    )
-    average_num_chars_per_qn = int(average_num_chars_per_qn)
-
-    average_num_words_per_qn = (
-        total_num_of_words_in_qn_body / total_num_qns if total_num_qns != 0 else 0
-    )
-    average_num_words_per_qn = int(average_num_words_per_qn)
-
-    num_of_q_type = len(q_type_count_map)
-    num_of_unique_qns = len(q_body_count_map)
-
-    stats: Dict = {
-        "Number of video sections found            ": num_video_sections,
-        "Number of video sections ignored          ": num_video_ignore,
-        "Number of videos Labelled                 ": num_video_labelled,
-        "Number of videos to be re-trimmed         ": num_video_require_retrim,
-        "Number of videos has critical point       ": num_video_has_critical_point,
-        "Total number of questions                 ": total_num_qns,
-        # "total_num_ops": total_num_ops,
-        "Average Num of questions per video        ": average_num_qns_per_video,
-        "Average Num of options per question       ": average_num_ops_per_qn,
-        "Average Num of characters per question    ": average_num_chars_per_qn,
-        "Average Num of words per question         ": average_num_words_per_qn,
-        "Number of question types                  ": num_of_q_type,
-        "Number of unique questions                ": num_of_unique_qns,
-        "Question Count per question types": q_type_count_map,
-        # "q_body_count_map": q_body_count_map,
-    }
-    return stats
-
-
 def parse_qa_label_txt(
     txt_fp: str, writeToJson: bool = False, local_qa_bank_fp: str = ""
 ) -> List[Dict]:
@@ -464,7 +381,7 @@ def parse_qa_label_txt(
         )
 
     if writeToJson and label_file_is_valid:
-        export_fp: Path = txt_fp.parent / "qa_label.json"
+        export_fp: Path = txt_fp.parent / f"{txt_fp.stem}.json"
         export_fp: Path = bump_version(export_fp)
 
         with export_fp.open(mode="w") as f:
@@ -475,7 +392,7 @@ def parse_qa_label_txt(
         print()
         logger.warning(f"No json file is generated.")
 
-    return qa_label_lst
+    return qa_label_lst, label_file_is_valid
 
 
 if __name__ == "__main__":
